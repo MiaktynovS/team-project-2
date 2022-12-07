d61d27bf6bab29916b3e82040ab6a358cca407d5
Oleksiy Frolov <avralgroup@gmail.com>
2022-12-07 14:56:35 +0200

    fetch

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { fetchCocktails } from './js/fetchCocktails';
+import { createCocktail } from './js/createCocktail';
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.btn-load-more'),
+}
+
+// let query = '';
+// let page = 1;
+// let simpleLightBox;
+// const perPage = 40;
+
+refs.searchForm.addEventListener('submit', onSearchForm);
+// refs.loadMoreBtn.addEventListener('click', onLoadBtn);
+
+function onSearchForm(event) {
+  event.preventDefault();
+  page = 1;
+  query = event.currentTarget.searchQuery.value.trim();
+  refs.gallery.innerHTML = '';
+
+
+  fetchCocktails(query).then(data => {
+    console.log(data.drinks);
+    createCocktail(data.drinks)
+
+  })
+}
